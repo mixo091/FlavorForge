06aecae6acc832ab2ad2728ad9c76461c63e33d4
mixo091 <mmichopoulos1.1@gmail.com>
2024-02-20 13:26:33 +0200

    react router set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import {BrowserRouter as Router , Routes , Route } from "react-router-dom"
+import Home from './pages/home';
+import CreateRecipe from './pages/create-recipe';
+import SavedRecipes from './pages/saved-recipes';
+import Auth from './pages/auth';
 
 function App() {
   return (
     <div className="App">
-        Recipe App
+        <Router>
+          <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/auth' element={<Auth/>}/>
+            <Route path='/create-recipe' element={<CreateRecipe/>}/>
+            <Route path='/saved-recipe' element={<SavedRecipes/>}/>
+          </Routes>
+        </Router>
     </div>
   );
 }
