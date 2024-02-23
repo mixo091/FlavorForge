9c78ce99dcb6d8281b997e2806a4f9a303928ce3
mixo091 <mmichopoulos1.1@gmail.com>
2024-02-23 10:09:20 +0200

    Created Recipe Model

---
+++ server/models/Recipes.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose'
+
+
+const RecipeSchema = new mongoose.Schema({
+    title : {type:String , required:true , unique:true},
+    ingredients : [{type:String  ,required:true}], 
+    instructions : {type:String , required:true },
+    imageUrl : {type:String , required:true },
+    cookingTime : {type:Number , required:true },
+    userOwner: { type: mongoose.Schema.Types.ObjectId , ref: 'users' , required:true }
+});
+
+export const RecipeModel = mongoose.model('recipes' , RecipeSchema);
