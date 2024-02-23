4ecddbd57fbcf923ba93cabaa91f15c836228cdc
mixo091 <mmichopoulos1.1@gmail.com>
2024-02-23 10:15:02 +0200

    Recipes Router set up

---
+++ server/routes/recipes.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { RecipeModel } from "../models/Recipes.js";
+import express from 'express'
+
+
+
+
+const router =  express.Router();
+
+
+
+
+
+
+export { router as recipesRouter };
