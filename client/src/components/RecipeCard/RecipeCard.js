d569a2ef9bedf05a068b4fed6dcd3b94730a1766
mixo091 <mmichopoulos1.1@gmail.com>
2024-03-14 09:48:43 +0200

    Recipe card Styling

--- client/src/components/RecipeCard/RecipeCard.js
+++ client/src/components/RecipeCard/RecipeCard.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { IoIosTimer } from "react-icons/io";
+import { BsSave2 } from "react-icons/bs";
 import './RecipeCard.css';
-import { useState } from 'react';
 
-const RecipeCard = ({ recipe ,saveRecipe ,isRecipeSaved }) => {
+const RecipeCard = ({ recipe, saveRecipe, isRecipeSaved }) => {
   const [showPopup, setShowPopup] = useState(false);
 
   const handleTogglePopup = () => {
@@ -14,25 +15,42 @@ const RecipeCard = ({ recipe ,saveRecipe ,isRecipeSaved }) => {
       <div className="recipe-card" onClick={handleTogglePopup}>
         <div className="recipe-header">
           {recipe.imageUrl && <img src={recipe.imageUrl} alt="Recipe" />}
-          <h3>{recipe.title}</h3>
+          <div className='recipe-small-info'>
+            <div>{recipe.title}</div>
+            <div className='recipe-small-time'>
+              <IoIosTimer />
+              <div>{recipe.cookingTime}'mins</div>
+            </div>
+            <div className='recipe-small-saved'>
+
+              {isRecipeSaved(recipe._id) ?  <div><BsSave2 /><div>Saved</div></div>: <div onClick={()=>{saveRecipe(recipe._id)}} ><BsSave2 /><div>Save</div></div>}
+            </div>
+           
+          </div>
         </div>
       </div>
+
       {showPopup && (
-        <div className="popup">
-          <div className="popup-content">
-          <button
+        <div className="popup-overlay">
+          <div className="popup">
+            <button className="close-btn" onClick={handleTogglePopup}>
+              &times;
+            </button>
+            <div className="popup-content">
+              <button
+                className="save-btn"
                 onClick={() => saveRecipe(recipe._id)}
                 disabled={isRecipeSaved(recipe._id)}
               >
-                {isRecipeSaved(recipe._id) ? "Saved" : "Save"}
-          </button>
-            <img src={recipe.imageUrl} alt="Recipe" />
-            <h2>{recipe.title}</h2>
-            <p>Description: {recipe.description}</p>
-            <p>Cooking Time: {recipe.cookingTime} minutes</p>
-            <p>Ingredients: {recipe.ingredients.join(', ')}</p>
-            <p>Instructions: {recipe.instructions}</p>
-            <button onClick={handleTogglePopup}>Close</button>
+                {isRecipeSaved(recipe._id) ? 'Saved' : 'Save'}
+              </button>
+              <img src={recipe.imageUrl} alt="Recipe" />
+              <h2>{recipe.title}</h2>
+              <p>Description: {recipe.description}</p>
+              <p>Cooking Time: {recipe.cookingTime} minutes</p>
+              <p>Ingredients: {recipe.ingredients.join(', ')}</p>
+              <p className="instructions">Instructions: {recipe.instructions}</p>
+            </div>
           </div>
         </div>
       )}
--- client/src/pages/CreateRecipe/CreateRecipe.jsx
+++ client/src/pages/CreateRecipe/CreateRecipe.jsx
@@ -10,6 +10,7 @@ import "./CreateRecipe.css";
 const CreateRecipe = () => {
   const userID = useGetUserID();
   const [cookies, _] = useCookies(["access_token"]);
+  const [step, setStep] = useState(1);
   const [recipe, setRecipe] = useState({
     title: "",
     description: "",
@@ -19,6 +20,7 @@ const CreateRecipe = () => {
     cookingTime: 0,
     userOwner: userID,
   });
+  const [errors, setErrors] = useState({});
 
   const navigate = useNavigate();
 
@@ -35,7 +37,6 @@ const CreateRecipe = () => {
 
   const handleAddIngredient = () => {
     setRecipe({ ...recipe, ingredients: [...recipe.ingredients, ""] });
-    console.log(recipe);
   };
 
   const handleRemoveIngredient = (index) => {
@@ -46,7 +47,6 @@ const CreateRecipe = () => {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    console.log(recipe);
     try {
       await axios.post(
         "http://localhost:5000/recipes/",
@@ -63,85 +63,150 @@ const CreateRecipe = () => {
     }
   };
 
+  const validateStep = () => {
+    const currentStepErrors = {};
+    switch (step) {
+      case 1:
+        if (!recipe.title || !recipe.description) {
+          currentStepErrors.title = !recipe.title ? "Title is required" : "";
+          currentStepErrors.description = !recipe.description ? "Description is required" : "";
+        }
+        break;
+      case 2:
+        if (recipe.ingredients.some(ingredient => !ingredient)) {
+          currentStepErrors.ingredients = "All ingredients must be filled";
+        }
+        break;
+      case 3:
+        if (!recipe.instructions || !recipe.imageUrl || !recipe.cookingTime) {
+          currentStepErrors.instructions = !recipe.instructions ? "Instructions are required" : "";
+          currentStepErrors.imageUrl = !recipe.imageUrl ? "Image URL is required" : "";
+          currentStepErrors.cookingTime = !recipe.cookingTime ? "Cooking Time is required" : "";
+        }
+        break;
+      default:
+        break;
+    }
+    setErrors(currentStepErrors);
+    return Object.keys(currentStepErrors).length === 0;
+  };
+
+  const nextStep = () => {
+    if (validateStep()) {
+      setStep(step + 1);
+    }
+  };
+
+  const prevStep = () => {
+    setStep(step - 1);
+  };
+
+  const renderStep = () => {
+    switch (step) {
+      case 1:
+        return (
+          <>
+            <div className="form-group">
+              <label htmlFor="title">Title</label>
+              <input
+                type="text"
+                id="title"
+                name="title"
+                value={recipe.title}
+                onChange={handleChange}
+              />
+              {errors.title && <div className="error">{errors.title}</div>}
+            </div>
+            <div className="form-group">
+              <label htmlFor="description">Description</label>
+              <textarea
+                id="description"
+                name="description"
+                value={recipe.description}
+                onChange={handleChange}
+              ></textarea>
+              {errors.description && <div className="error">{errors.description}</div>}
+            </div>
+            <button onClick={nextStep}>Next</button>
+          </>
+        );
+      case 2:
+        return (
+          <>
+            <label htmlFor="ingredients">Ingredients</label>
+            {recipe.ingredients.map((ingredient, index) => (
+              <div key={index} className="ingredient">
+                <input
+                  type="text"
+                  value={ingredient}
+                  onChange={(e) => handleIngredientChange(index, e.target.value)}
+                />
+                <button
+                  type="button"
+                  onClick={() => handleRemoveIngredient(index)}
+                >
+                  <FiDelete />
+                </button>
+              </div>
+            ))}
+            <button type="button" onClick={handleAddIngredient}>
+              Add ingredient <IoMdAddCircleOutline />
+            </button>
+            {errors.ingredients && <div className="error">{errors.ingredients}</div>}
+            <button onClick={prevStep}>Previous</button>
+            <button onClick={nextStep}>Next</button>
+          </>
+        );
+      case 3:
+        return (
+          <>
+            <div className="form-group">
+              <label htmlFor="instructions">Instructions</label>
+              <textarea
+                className="instructions"
+                id="instructions"
+                name="instructions"
+                value={recipe.instructions}
+                onChange={handleChange}
+              ></textarea>
+              {errors.instructions && <div className="error">{errors.instructions}</div>}
+            </div>
+            <div className="form-group">
+              <label htmlFor="imageUrl">Image URL</label>
+              <input
+                type="text"
+                id="imageUrl"
+                name="imageUrl"
+                value={recipe.imageUrl}
+                onChange={handleChange}
+              />
+              {errors.imageUrl && <div className="error">{errors.imageUrl}</div>}
+            </div>
+            <div className="form-group">
+              <label htmlFor="cookingTime">Cooking Time (minutes)</label>
+              <input
+                type="number"
+                id="cookingTime"
+                name="cookingTime"
+                value={recipe.cookingTime}
+                onChange={handleChange}
+              />
+              {errors.cookingTime && <div className="error">{errors.cookingTime}</div>}
+            </div>
+            <button onClick={prevStep}>Previous</button>
+            <button type="submit">Create Recipe</button>
+          </>
+        );
+      default:
+        return null;
+    }
+  };
+
   return (
     <div className="create-recipe-container">
       <form className="create-recipe-form" onSubmit={handleSubmit}>
         <h2>Create Recipe</h2>
-        <div className="recipe-form-group">
-          <label htmlFor="title">Title</label>
-          <input
-            type="text"
-            id="title"
-            name="title"
-            value={recipe.title}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="recipe-form-group">
-          <label htmlFor="description">Description</label>
-          <textarea
-            id="description"
-            name="description"
-            value={recipe.description}
-            onChange={handleChange}
-          ></textarea>
-        </div>
-      
-        <label htmlFor="ingredients">Ingredients</label>
-        {recipe.ingredients.map((ingredient, index) => (
-          <div key={index} className="ingredient">
-            <input
-              type="text"
-              value={ingredient}
-              onChange={(e) => handleIngredientChange(index, e.target.value)}
-            />
-            <button
-              type="button"
-              onClick={() => handleRemoveIngredient(index)}
-            >
-              <FiDelete />
-            </button>
-          </div>
-        ))}
-        <button type="button" onClick={handleAddIngredient}>
-          Add ingredient <IoMdAddCircleOutline />
-        </button>
-
-
-        <div className="recipe-form-group">
-          <label htmlFor="instructions">Instructions</label>
-          <textarea
-            className="instructions"
-            id="instructions"
-            name="instructions"
-            value={recipe.instructions}
-            onChange={handleChange}
-          ></textarea>
-        </div>
-
-
-        <div className="recipe-form-group">
-          <label htmlFor="imageUrl">Image URL</label>
-          <input
-            type="text"
-            id="imageUrl"
-            name="imageUrl"
-            value={recipe.imageUrl}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="recipe-form-group">
-          <label htmlFor="cookingTime">Cooking Time (minutes)</label>
-          <input
-            type="number"
-            id="cookingTime"
-            name="cookingTime"
-            value={recipe.cookingTime}
-            onChange={handleChange}
-          />
-        </div>
-
-        <button type="submit">Create Recipe</button>
+        {renderStep()}
       </form>
     </div>
   );
